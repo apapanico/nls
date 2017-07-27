4982c12415f3ac74c8723821018f7addd8e41c72
Kreol64 <ivanomir4@yahoo.com>
2017-07-27 15:50:39 -0500

    added all currently available estimators; changed how a hashtable is filled

--- simulations.py
+++ simulations.py
@@ -2,23 +2,36 @@
 import numpy as np
 import click
 
-import matplotlib
-matplotlib.use('qt5agg')
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
+from cov.nlshrink import nlshrink_eig
+from cov.nlshrink import nlshrink_covariance
+
+from utils import *
+from models import *
+from shrinkage import *
 
 
-from scipy.sparse.linalg import eigsh
 from collections import defaultdict
 from pylab import setp
+from tqdm import tqdm
 
 uniform = np.random.uniform 
 eigvals = np.linalg.eigvals
 
-### Demonstrate the downward/upward bias for sample cov matrix under various covariance models
-### There are 2 sources of randomness in this code: 
-### 1. Seed for generate_simple_returns 
-### 2. Seed for generate_uniform_volatilities
+
+''' 
+
+This module demonstrates the downward/upward bias in eigenvalues 
+for sample cov matrix as well as for other estimators.
+////////////////////////////////////////////////////////////////////
+                    Available estimators : 
+'sample','minvar_nls_oracle','minvar_nls_kfold_oracle','nls_kfold',
+'nls_loo','nls_oracle','minvar_nls_oracle_reg','nls_asymptotic',
+'minvar_nls_kfold','minvar_nls_loo'
+
+''' 
+
 
 def generate_simple_returns(vols,T,seed=None):
    
@@ -40,63 +53,57 @@ def generate_uniform_volatilities(N,seed=None):
     # N - number of securities
     tdays = 256 
     rng  = np.random.RandomState(seed)
-    vols = rng.uniform(((5/100)**2)/tdays,((30/100)**2)/tdays,N).tolist()   # assume 5-30% fluctuation a year
+    vols = rng.uniform(((5/100)**2)/tdays,((30/100)**2)/tdays,N).tolist()   # assume 5-30% fluctuation a year. Those are daily volatilities
     
     return vols
  #@def
- 
-def cov_mat_sample(R):
     
-    T = R.shape[0]
-    vcov = R.T.dot (R) / T
+def simulate_eigs(simulations=5,N=10,T=100,seed=1,estimators=['sample']):
     
-    return vcov
-#@def
-
-def cov_mat_nls(R):
-    return "not implemented yet"
-#@def
-
-def cov_mat_cv(R):
-    return "not implemented yet"
-#@def
-
-def eig(A, k, return_eigenvectors=False):
-    
-    #extract eigenvalues with sparse lin algebra
-    
-    # A - matrix
-    # k - number of eigenvalues to be extracted (<A.shape[1])
-        
-    result = eigsh(A, k, which='LA', maxiter=int(1e9),
-                   return_eigenvectors=return_eigenvectors)
-    if return_eigenvectors:
-        vals, vecs = result
-        return vals, vecs
-    else:
-        vals = result
-        if k == 1:
-            vals = vals[0]
-        return vals
-#@def
-    
-def simulate_eigs(simulations=1000,N=100,T=1000,seed=1):
-    
-    # this function calculates eigenvalues for every simulated matrix 
+    # this function calculates eigenvalues for different estimators and for every simulated matrix 
     # and stores the result in a hashtable 
+    
+    ''' currently supported estimators ''' 
     
     seeds        = range(seed, seed + simulations)
     eigenvalues  = defaultdict(list)
     vols         = np.sort(generate_uniform_volatilities(N,seed=seed))[::-1]
+    Sigma        = np.diag(vols)
+    
+    pbar = tqdm(total=simulations)
 
+    
     for i in range(simulations):
+        
         R    = generate_simple_returns(vols,T,seed=seeds[i])
-        C_sample    = cov_mat_sample(R)
-        #C_nls      = 
-        #C_cv       = 
-        eigenvalues['sample'+'_'+\
-                    'T='+str(T)+'_'+\
-                    'N='+str(N)].append(np.sort(eigvals(C_sample))[::-1])
+        
+        # calculate a sample covariance matrix
+        
+        S    = cov(R)
+        eig_sample,U = eig(S,return_eigenvectors=True)
+        
+        ''' Calculate eigenvalues with different estimators '''
+        
+        for est in estimators:
+            if est is 'sample':
+                eigenvalues['sample' +'_'+'T='+str(T)+'_'+ 'N='+str(N)].append(eig_sample)
+            else:
+                kwargs = dict(X=R, S=S,U=U) 
+                if 'oracle' in est:
+                    kwargs['Sigma'] = Sigma
+                if 'kfold' in est:
+                    kwargs['K'] = 10
+                if 'minvar' in est and 'reg' not in est and 'loo' not in est:
+                    kwargs['lam'] = eig_sample
+                if 'reg' in est:
+                    kwargs['lmbda'] = 0.000005
+                
+                eigenvalues[est +'_'+'T='+str(T)+'_'+ 'N='+str(N)].append(estimators_functions[est](**kwargs)[1])
+            #@if
+        #@for
+            
+  
+        pbar.update()
     #@for
         
     return eigenvalues,vols
@@ -106,6 +113,8 @@ def read_inquiry(inq):
     # inq = inquiry of the form (estimator,T,N)
     return inq[0]+'_T='+str(inq[1])+'_N='+str(inq[2])
 #@def
+
+
 
 def box_plot_eigs(simResult,T=1000,N=100,estimators=['sample'],qtile=5):
     
@@ -119,16 +128,19 @@ def box_plot_eigs(simResult,T=1000,N=100,estimators=['sample'],qtile=5):
     
     # convert daily variances into annualized std in % 
     
-    tdays = 256 
-    sd_annual = lambda x: np.sqrt(x*tdays)*100
-    
     true_eigs = sd_annual(simResult[1])
         
     for i in estimators:
         inq = read_inquiry((i,T,N))
-        est_eigs  = np.vstack(simResult[0][inq])
-        est_eigs  = sd_annual(est_eigs)
-    
+        if inq not in simResult[0].keys():
+            raise Exception('No simulations run for '+inq+' estimator')
+        else:
+            est_eigs  = np.vstack(simResult[0][inq])
+            est_eigs  = sd_annual(est_eigs)
+        #@if
+        
+        print(est_eigs)
+
         # choose eigenvalues on qtile quantiles
         xcoord      = np.rint(np.linspace(0,N-1,qtile)).astype(int)
         box_medians = np.median(est_eigs,0)
@@ -176,7 +188,7 @@ def box_plot_eigs(simResult,T=1000,N=100,estimators=['sample'],qtile=5):
         
         # draw titles, axes and etc.
         
-        plt.title('N = ' + str(N)+', T= ' + str(T), y=1.01,
+        plt.title('Estimator= '+ i + ', N = ' + str(N)+', T= ' + str(T), y=1.01,
                   fontsize=18, color='black')
         plt.xticks(list(range(1,qtile+1)),
                    ['${\\lambda_{'+ str(i) + '}(\hat{\Sigma})}$' for i in xcoord+1])
@@ -208,8 +220,8 @@ def box_plot_eigs(simResult,T=1000,N=100,estimators=['sample'],qtile=5):
                              markersize=10,
                              label='Bias')
 
-        legend = plt.legend(handles=[vleg,mleg],
-                            numpoints=1, fancybox=True, framealpha=0.25)
+        plt.legend(handles=[vleg,mleg],
+                   numpoints=1, fancybox=True, framealpha=0.25)
         
         plt.show()
         #plt.clf()
@@ -218,18 +230,20 @@ def box_plot_eigs(simResult,T=1000,N=100,estimators=['sample'],qtile=5):
     
 #@def
 
+
 @click.command()
-@click.option('--n', type=int, default=10)
-@click.option('--t', type=int, default=100)
-@click.option('--simulations', type=int, default=1000)
-@click.option('--seed', type=int, default=1)
-@click.option('--qtile', type=int, default=5)
-@click.option('--estimators', type=list, default=['sample'])
+@click.option('--n', type=int, default=50)
+@click.option('--t', type=int, default=200)
+@click.option('--simulations', type=int, default=50)
+@click.option('--seed', type=int, default=2)
+@click.option('--qtile', type=int, default=10)
+@click.option('--estimators', type=list, default=['sample','minvar_nls_kfold_oracle','minvar_nls_oracle_reg'])
 
 def main(n,t,simulations,seed,qtile,estimators):
-    simResult = simulate_eigs(simulations=simulations,N=n,T=t,seed=seed)
+    simResult = simulate_eigs(simulations=simulations,N=n,T=t,seed=seed,estimators=estimators)
     box_plot_eigs(simResult,estimators=estimators,qtile=qtile,T=t,N=n)  
 #@main
 
 if __name__ == "__main__":
     main()
+
